512cad239c1592355172bd1e728b66ee7d1d8366
Zach Levitt <zlevitt@middlebury.edu>
2019-10-30 00:56:31 -0400

    last night

--- divestment/script.js
+++ divestment/script.js
@@ -1,8 +1,25 @@
 function myFunction(name) {
+
   var x = document.getElementById(name);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+
+
+}
+
+function myFunction2(name) {
+
+  var y = document.getElementById("full")
+  y.style.display = "none"
+  var z = document.getElementById("partial")
+  z.style.display = "none"
+  var w = document.getElementById("coal")
+  w.style.display = "none"
+  
+  var x = document.getElementById(name);
+  x.style.display = "block";
+  console.log(name)
 }
